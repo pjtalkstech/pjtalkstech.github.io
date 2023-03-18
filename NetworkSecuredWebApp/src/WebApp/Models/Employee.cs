87f674cb35c9663c92e37ef19b7e62e07f3d5461
Pavan <chintupawan@gmail.com>
2023-03-19 12:36:08 +1300

    removed unnecessary pages

--- NetworkSecuredWebApp/src/WebApp/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApp.Models
-{
-    public class Employee
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
